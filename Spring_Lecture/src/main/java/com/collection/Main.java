d3aa56dde693153ae73bd8708bf224a1ddc78bbf
C <viranidhruv002@gmail.com>
2023-05-17 21:57:45 +0530

    Done ref

--- Spring_Lecture/src/main/java/com/collection/Main.java
+++ Spring_Lecture/src/main/java/com/collection/Main.java
@@ -8,6 +8,10 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/collection/config.xml");
 		Student s1 = (Student) context.getBean("s1");
-		System.out.println(s1);
+		System.out.println(s1.getId());
+		System.out.println(s1.getName());
+		System.out.println(s1.getMobile());
+		System.out.println(s1.getAddress());
+		System.out.println(s1.getCourse());
 	}
 }
